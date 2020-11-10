3aec4bf14f524dc07d223c10f18ad742baf25f43
rish07 <rishirajsinghchhabra@gmail.com>
2020-11-10 20:21:00 +0530

    Returning JSON response

--- main.go
+++ main.go
@@ -1,17 +1,35 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
+type Article struct {
+	Title   string `json:"Title"`
+	Desc    string `json:"Desc"`
+	Content string `json:"Content"`
+}
+
+type Articles []Article
+
+func allArticles(w http.ResponseWriter, r *http.Request) {
+	articles := Articles{
+		Article{Title: "Test Title", Desc: "Test Desc", Content: "This is the first Article"},
+	}
+
+	fmt.Println(w, "Endpoint Hit: allArticles")
+	json.NewEncoder(w).Encode(articles)
+}
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Homepage Endpoint")
 }
 
 func handleRequests() {
 	http.HandleFunc("/", homePage)
+	http.HandleFunc("/articles", allArticles)
 	log.Fatal(http.ListenAndServe(":8082", nil))
 }
 
